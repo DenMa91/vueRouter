43836a7c4f1be6d9cc6b56972bb3d510f7ddcc6b
Mattia Denegri <mattia.denegri@gmail.com>
2021-12-19 17:41:56 +0100

    aggiunto data fetch in job.vue e jobDetails.vue

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -2,8 +2,9 @@
   <section class="container">
       <div class="row">
           <div class="col-12">
-              <h1>Job Details</h1>
+              <h1>{{job.title}}</h1>
               <p>the job id is {{id}}</p>
+              <p>{{job.details}}</p>
           </div>
       </div>
   </section>
@@ -14,8 +15,14 @@ export default {
     props:['id'],
     data() {
         return {
-           
+           job:[]
         }
+    },
+    mounted() {
+        fetch("http://localhost:3000/jobs/" + this.id)
+        .then((response)=> response.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
     },
 }
 </script>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -2,14 +2,16 @@
   <section class="container">
       <div class="row">
               <h1 class="text-center">Jobs</h1>
-                  <div class="card col-4" v-for="job in jobs" :key="job.id">
+                <div class="p-3 col-4 cardContainer"  v-for="job in jobs" :key="job.id">
+                  <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">{{job.title}}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{job.id}}</h6>
                         <p class="card-text">job.details</p>
                         <router-link :to="{name:'JobDetails', params:{id: job.id}}" class="card-link">scopri di piu</router-link>
                     </div>
-                  </div>          
+                  </div>
+                </div>          
           </div>
   </section>
 </template>
@@ -18,12 +20,14 @@
 export default {
     data() {
         return {
-            jobs: [
-                {title: 'UX Designer', id:1, details:'lorem 1'},
-                {title: 'WEB Developer', id:2, details:'lorem 2'},
-                {title: 'VUE Developer', id:3, details:'lorem 3'}
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+        fetch("http://localhost:3000/jobs")
+        .then((response)=> response.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     },
 }
 </script>
