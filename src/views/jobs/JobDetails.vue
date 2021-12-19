ca96c3a219b82067ddafa11a4b2831490af9ce5e
Mattia Denegri <mattia.denegri@gmail.com>
2021-12-19 16:55:39 +0100

    aggiunta rotta jobs e rotta parametrica job/id e relative viste

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,22 @@
 <template>
-  
+  <section class="container">
+      <div class="row">
+          <div class="col-12">
+              <h1>Job Details</h1>
+              <p>the job id is {{id}}</p>
+          </div>
+      </div>
+  </section>
 </template>
 
 <script>
 export default {
-
+    props:['id'],
+    data() {
+        return {
+           
+        }
+    },
 }
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -7,7 +7,7 @@
                         <h5 class="card-title">{{job.title}}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{job.id}}</h6>
                         <p class="card-text">job.details</p>
-                        <a href="#" class="card-link">scopri di piu</a>
+                        <router-link :to="{name:'JobDetails', params:{id: job.id}}" class="card-link">scopri di piu</router-link>
                     </div>
                   </div>          
           </div>
